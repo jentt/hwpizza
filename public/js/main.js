bc5a547e632ba512ec3fd2c71ca11c6ae324fa37
Jennifer Nguyen <jennifernguyen@Jennifers-MacBook-Pro.local>
2021-08-12 23:51:21 -0600

    Added scrollToTopBtn

--- public/js/main.js
+++ public/js/main.js
@@ -35,4 +35,20 @@ function toggleMenu() {
   }
 }
 
+// Scroll back to top button
+var btn = $('#scrollToTopBtn');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
 
